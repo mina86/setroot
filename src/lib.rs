3921f11992787f398205dba8def2445341f662df
Michal Nazarewicz <mina86@mina86.com>
2025-11-17 13:29:53 +0100

    Fix copyright notice

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 // A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License along with
-// Foobar.  If not, see <http://www.gnu.org/licenses/>.
+// setroot.  If not, see <http://www.gnu.org/licenses/>.
 
 #![doc = include_str!("../README.md")]
 
